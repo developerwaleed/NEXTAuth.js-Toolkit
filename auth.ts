6f1544237512778c1318afe89d2301bd54e0b4b4
Waleed Amjad <waleedamjad99@hotmail.com>
2024-01-23 13:39:59 +0500

    edit token to add custom fields

--- auth.ts
+++ auth.ts
@@ -1,8 +1,11 @@
 import NextAuth from "next-auth";
 import { PrismaAdapter } from "@auth/prisma-adapter";
 import authConfig from "./auth.config";
+import { getUserById } from "./data/user";
 
 import { db } from "./lib/db";
+import { UserRole } from "@prisma/client";
+
 
 export const {
   handlers: { GET, POST },
@@ -12,17 +15,24 @@ export const {
 } = NextAuth({
   callbacks: {
     async session({ token, session }) {
-      console.log({
-        sessionToken: token,
-        session,
-      });
       if (token.sub && session.user) {
         session.user.id = token.sub;
+      }
+      if (token.role && session.user) {
+        session.user.role = token.role as UserRole;
       }
       return session;
     },
 
     async jwt({ token }) {
+      if (!token.sub) return token;
+
+      const existingUser = await getUserById(token.sub);
+
+      if (!existingUser) return token;
+
+      token.role = existingUser.role;
+
       return token;
     },
   },
