c5a6ae93c6d484b4cda7184d52ac22fd87b87d78
Waleed Amjad <waleedamjad99@hotmail.com>
2024-01-23 10:23:31 +0500

    enchance actions

--- actions/login.ts
+++ actions/login.ts
@@ -1,16 +1,35 @@
-'use server';
+"use server";
 import * as z from "zod";
 import { LoginSchema } from "@/schemas";
-
+import { signIn } from "@/auth";
+import { DEFAULT_LOGIN_REDIRECT } from "@/routes";
+import { AuthError } from "next-auth";
 export const login = async (values: z.infer<typeof LoginSchema>) => {
-    const validatedFields = LoginSchema.safeParse(values);
+  const validatedFields = LoginSchema.safeParse(values);
 
-    if(!validatedFields.success)
-    {
-        return {
-            error:"Invalid Fields!"
-        };
+  if (!validatedFields.success) {
+    return {
+      error: "Invalid Fields!",
+    };
+  }
+
+  const { email, password } = validatedFields.data;
+
+  try {
+    await signIn("credentials", {
+      email,
+      password,
+      redirectTo: DEFAULT_LOGIN_REDIRECT,
+    });
+  } catch (error) {
+    if (error instanceof AuthError) {
+      switch (error.type) {
+        case "CredentialsSignin":
+          return { error: "invalid Credentials!" };
+        default:
+          return { error: "Something went wrong!" };
+      }
     }
-
-    return {success: "Email Sent"}
-}
+    throw error;
+  }
+};
