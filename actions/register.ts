6e3d603367ca7b657cb6533cd147b8bfc1de71ab
Waleed Amjad <waleed.cortechsole@gmail.com>
2024-01-31 19:08:07 +0500

    send verification email or registration

--- actions/register.ts
+++ actions/register.ts
@@ -4,6 +4,8 @@ import { RegisterSchema } from "@/schemas";
 import bcrypt from "bcryptjs";
 import { db } from "@/lib/db";
 import { getUserByEmail } from "@/data/user";
+import { generateVerificationToken } from "@/lib/tokens";
+import { sendVerificationEmail } from "@/lib/mail";
 
 export const register = async (values: z.infer<typeof RegisterSchema>) => {
   const validatedFields = RegisterSchema.safeParse(values);
@@ -15,7 +17,7 @@ export const register = async (values: z.infer<typeof RegisterSchema>) => {
   }
   const { name, email, password } = validatedFields.data;
   const hashedPassword = await bcrypt.hash(password, 10);
-  
+
   const existingUser = await getUserByEmail(email);
 
   if (existingUser) {
@@ -32,5 +34,8 @@ export const register = async (values: z.infer<typeof RegisterSchema>) => {
     },
   });
 
-  return { success: "User Created!" };
+  const verificationToken = await generateVerificationToken(email);
+  await sendVerificationEmail(verificationToken.email, verificationToken.token);
+
+  return { success: "Confirmation email sent!" };
 };
