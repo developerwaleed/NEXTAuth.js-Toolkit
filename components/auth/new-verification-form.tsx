b1a43eae54a8460fbe705244fc13727c84da6db6
Waleed Amjad <waleedamjad99@hotmail.com>
2024-02-08 17:10:42 +0500

    Add email verification Page

---
+++ components/auth/new-verification-form.tsx
@@ -0,0 +1,10 @@
+"use client";
+
+export const NewVerificationForm = () => {
+return (
+    <div>
+        <h1>Verify your email</h1>
+        <p>Check your email for a link to verify your account. If it doesn't appear within a few minutes, check your spam folder.</p>
+    </div>
+)
+}
